cbf586f0051597f2723c47ae2bbca6393c49cb54
Shubham1943 <32012551+Shubham1943@users.noreply.github.com>
2023-10-23 13:38:26 +0530

    Update checkout.py

--- checkout.py
+++ checkout.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
 
+driver=webdriver.Chrome(executable_path="D:\\Documents\\chromwdriver.exe")
+driver.maximize_window()
+driver.get("https://www.saucedemo.com/")
+driver.find_element_by_path("//input[@id='user-name']").send_keys="standard_user"
+driver.find_element_by_path("//input[@id='password']").send_keys="secret_sauce"
+driver.find.element_by_path("//input[@id='login-button']").click()
+time.sleep(5)
+
+def checkout():
+  if(driver.find_element_by_xpath("//*[@id="header_container"]/div[2]/span).is_displayed()):
+    driver.find.element_by_path("//*[@id="checkout"]").click()
+    if(driver.find.element_by_path("//*[@id="header_container"]/div[2]/span")):
+      driver.find.element_by_path("//*[@id="continue"]").click()
+  else:
+  print("Error in loading")
+  
