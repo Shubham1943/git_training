ff7bfb7b74e6027fad90524377c7f06ab32bdc59
Shubham1943 <32012551+Shubham1943@users.noreply.github.com>
2023-10-28 17:50:25 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,26 +1,34 @@
-from selenium import webdriver
+*** Settings ***
 
-driver=webdriver.Chrome(executable_path="D:\\Documents\\chromwdriver.exe")
-driver.maximize_window()
-driver.get("https://www.saucedemo.com/")
-driver.find_element_by_path("//input[@id='user-name']").send_keys="standard_user"
-driver.find_element_by_path("//input[@id='password']").send_keys="secret_sauce"
-driver.find.element_by_path("//input[@id='login-button']").click()
-time.sleep(5)
-if(driver.find_element_by_xpath("//div[text()='Swag Labs']"].is_displayed()):
-  print("Successfully Loggedin")
-else:
-  print("Login failed")
-
-
-*** Setting ***
 Library SeleniumLibrary
 
 *** Variables ***
+${browser}  chrome
+${url}  xx-x-x--x\chromedriver.exe
 
 *** Test Cases ***
-Login Test
-  create webdriver  chrome  executavl
-  
-
+LoginTest
+    create webdriver    chrome  executable_path="xx-x-x--x\chromedriver.exe"
+    Open browser    ${url}  ${browser}
+    maximize browser window
+    input text      id:user-name    standard_user
+    input text      id:password     secret_sauce
+    click element   xpath://input[@id='login-button']
+    close browser
+    
 *** Keywords ***
+loginToApplication
+    create webdriver    chrome  executable_path="xx-x-x--x\chromedriver.exe"
+    Open browser    ${url}  ${browser}
+    maximize browser window
+    ${username}     set variable    id:user-name   
+    ${password}     set variable    id:password   
+    
+    element should be visible   ${username}  
+    element should be visible   ${password}
+    
+    sleep   5
+    
+    input text      ${username}     standard_user
+    input text      ${password}     secret_sauce
+    click element   xpath://input[@id='login-button']
