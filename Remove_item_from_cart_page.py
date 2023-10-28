0549448e8d8b93ec02284495f61a9e8fb7e77c16
Shubham1943 <32012551+Shubham1943@users.noreply.github.com>
2023-10-28 18:37:17 +0530

    Create Remove_item_from_cart_page.py

---
+++ Remove_item_from_cart_page.py
@@ -0,0 +1,26 @@
+*** Settings ***
+Library SeleniumLibrary
+
+*** Variables ***
+${browser}  chrome
+${url}  xx-x-x--x\chromedriver.exe
+
+*** Test Cases ***
+add_to_cart
+    create webdriver    chrome  executable_path="xx-x-x--x\chromedriver.exe"
+    Open browser    ${url}  ${browser}
+    maximize browser window
+    input text      id:user-name    standard_user
+    input text      id:password     secret_sauce
+    click element   xpath://input[@id='login-button']
+    click element   xpath://a[@class='shopping_cart_link']
+    title should be     'Your Cart'
+    
+    click element   xpath://input[@id='remove-sauce-labs-backpack']
+    click element   xpath://input[@id='remove-sauce-labs-bike-light']
+    click element   xpath://input[@id='remove-sauce-labs-bolt-t-shirt']
+    click element   xpath://input[@id='remove-sauce-labs-fleece-jacket']
+    click element   xpath://input[@id='remove-sauce-labs-onesie']
+    click element   xpath://input[@id='remove-test.allthethings()-t-shirt-(red)']
+    
+    click element   xpath://input[@id='//continue-shopping']
