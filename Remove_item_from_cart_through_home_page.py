6c0303433fb1c330122d81d5badb01463d280002
Shubham1943 <32012551+Shubham1943@users.noreply.github.com>
2023-10-29 12:29:33 +0530

    Update Remove_item_from_cart_through_home_page.py

--- Remove_item_from_cart_through_home_page.py
+++ Remove_item_from_cart_through_home_page.py
@@ -4,7 +4,8 @@ Library SeleniumLibrary
 *** Variables ***
 ${browser}  chrome
 ${url}  xx-x-x--x\chromedriver.exe
-
+@{cart_item}    add-to-cart-sauce-labs-backpack     add-to-cart-sauce-labs-bike-light       add-to-cart-sauce-labs-bolt-t-shirt     add-to-cart-sauce-labs-fleece-jacket        add-to-cart-sauce-labs-onesie       add-to-cart-test.allthethings()-t-shirt-(red)
+${string_condition}     Remove
 *** Test Cases ***
 add_to_cart
     create webdriver    chrome  executable_path="xx-x-x--x\chromedriver.exe"
@@ -14,12 +15,12 @@ add_to_cart
     input text      id:password     secret_sauce
     click element   xpath://input[@id='login-button']
     
-    click element   xpath://input[@id='remove-sauce-labs-backpack']
-    click element   xpath://input[@id='remove-sauce-labs-bike-light']
-    click element   xpath://input[@id='remove-sauce-labs-bolt-t-shirt']
-    click element   xpath://input[@id='remove-sauce-labs-fleece-jacket']
-    click element   xpath://input[@id='remove-sauce-labs-onesie']
-    click element   xpath://input[@id='remove-test.allthethings()-t-shirt-(red)']
+    FOR     ${ITEM}     IN      @{CART_ITEM}
+        
+        IF   "${string_condition}" == "Remove"
+            click element   ${ITEM}
+            LOG     "ITEM REMOVED FROM CART"
+    END 
     
     click element   xpath://a[@class='shopping_cart_link']
     
